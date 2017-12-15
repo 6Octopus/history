e8883296ee11d1a718436e095cafb739e970876e
James Terry <jamesnorthterry@gmail.com>
2017-12-14 19:33:59 -0800

    setting up some more

--- sampleSender.js
+++ sampleSender.js
@@ -1,8 +1,12 @@
 const axios = require('axios');
 
 axios.post('http://localhost:3000/viewed', {
-  'first': 'carlos',
-  'last': 'danger'
+  userID: 'UCRDXA8xNPF6K_Zxj5YYyfIg',
+  instanceID: 'pn01',
+  videoID: 'u2xGb6y3rBE',
+  isAutoplay: true,
+  progress: 'PT0S',
+  totalLength: 'PT40S'
 })
 .then(res => {
   console.log('Success: ' + res.status)
--- server/index.js
+++ server/index.js
@@ -10,7 +10,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json()); // honestly I don't think I'll ever use this
 
 app.get('/', (req, res) => {
-  dbHelper.simpler();
   res.send('Hello World!');
 });
 
